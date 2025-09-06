efe81cdb0f46d0618e8745f3fda65d4facfa6c8d
amarthaluriruthika <amarthaluriruthika@gmail.com>
2025-09-06 15:54:56 +0530

    learned functions in js

---
+++ functions.js
@@ -0,0 +1,26 @@
+function greet(){
+    console.log("Welcome to DevAstra")
+}
+greet()
+function add(x,y){
+    return x+y;
+}
+let result=add(10,20)
+console.log(result)
+function mul(x,y){
+    return x*y;
+}
+console.log(mul(10,20))
+const pi=3.14
+console.log(pi)
+const multiply=function(x,y){
+    return x*y;
+}
+console.log(multiply(5,3))
+const square=(x)=>x*x;
+console.log(square(6))
+const subtract=(x,y)=>{
+    console.log(x,y)
+    return x-y
+}
+console.log(subtract(20,14))
