c6e6a84f01527a898cc7ca9b5188ea2b0e9bfcde
amarthaluriruthika <amarthaluriruthika@gmail.com>
2025-09-06 12:34:15 +0530

    learned datatypes in js

---
+++ practice.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+let name ="DevAstra";
+console.log(typeof name)
+num=1000
+let isLogged=false;
+console.log (isLogged)
+console.log(num)
+let x;
+console.log(typeof x)
+let y=null;
+console.log(y)
+let arr=[1,2,6]
+console.log(typeof arr)
+console.log(typeof y)
+let person={
+    name:"Ruthika",
+    age:19,
+}
+console.log(typeof person)
